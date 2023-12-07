c86492deaeb54c9dfb40259d0cd2d9943513599d
fasbrothers <nigmatovsanjar00@gmail.com>
2023-12-07 12:50:01 +0500

    feat: add refresh token functionality

--- src/api/httpClient.tsx
+++ src/api/httpClient.tsx
@@ -4,9 +4,13 @@ import axios, {
   AxiosResponse,
   InternalAxiosRequestConfig,
 } from 'axios';
-import { store } from '../stores/store';
 import toast from 'react-hot-toast';
+import { store } from '../stores/store';
 import { ErrorResponse } from '../types/error.types';
+import { removeToken, setToken } from '../stores/slices/authSlice';
+import { RefreshResponse, RefreshingPromise, isRefreshingType } from '../types/auth.types';
+
+let isRefreshing: isRefreshingType = false;
 
 const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL as string,
@@ -34,6 +38,41 @@ function resInterceptor(response: AxiosResponse) {
 async function resErrInterceptor(error: AxiosError<ErrorResponse>) {
   if (error.response?.status === 401) {
     const originalRequest = error.config as AxiosRequestConfig;
+    if (isRefreshing) {
+      const res = await isRefreshing;
+
+      if (typeof res === 'object' && 'access_token' in res) {
+        return axios({
+          ...originalRequest,
+          headers: {
+            ...originalRequest.headers,
+            Authorization: `Bearer ${res.access_token}`,
+          },
+        });
+      }
+
+      return Promise.reject(error);
+    }
+
+    isRefreshing = refreshAccessToken().then((res) => {
+      if ('error' in res) {
+        store.dispatch(removeToken());
+        httpClient.defaults.headers.common['Authorization'] = '';
+        toast.error('Your session has expired. Please login again.');
+
+        return res;
+      }
+
+      store.dispatch(setToken(res.access_token));
+
+      if (!originalRequest.headers) originalRequest.headers = {};
+      originalRequest.headers['Authorization'] = `Bearer ${res.access_token}`;
+
+      return res;
+    });
+
+    await isRefreshing;
+    isRefreshing = false;
 
     return axios(originalRequest);
   } else {
@@ -43,7 +82,23 @@ async function resErrInterceptor(error: AxiosError<ErrorResponse>) {
   return Promise.reject(error);
 }
 
+const refreshAccessToken = async (): Promise<RefreshingPromise> => {
+  const userId = localStorage.getItem('sub');
+  try {
+    const response = await httpClient.get<RefreshResponse>(`/auth/${userId}/refresh`);
+
+    const {
+      tokens: { access_token },
+    } = response.data;
+
+    return { access_token };
+  } catch (error: unknown) {
+    return { error: error as Error };
+  }
+};
+
 httpClient.interceptors.request.use(reqInterceptor, reqErrInterceptor);
 httpClient.interceptors.response.use(resInterceptor, resErrInterceptor);
 
 export default httpClient;
+export { refreshAccessToken };
--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,3 +1,15 @@
 export type AuthState = {
   token: string | null;
 };
+
+export type RefreshingPromise = { access_token: string } | { error: Error };
+
+export type isRefreshingType = Promise<RefreshingPromise> | boolean;
+
+export interface RefreshResponse {
+  tokens: {
+    access_token: string;
+    refresh_token: string;
+  };
+  message: string;
+}
