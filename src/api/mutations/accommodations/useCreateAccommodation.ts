3aa5f3c395f77afb041f60d61aa7921087151cd2
Zach13131 <125944644+Zach13131@users.noreply.github.com>
2024-01-11 21:31:59 +0500

    feat(add): toasts to create and update requests

--- src/api/mutations/accommodations/useCreateAccommodation.ts
+++ src/api/mutations/accommodations/useCreateAccommodation.ts
@@ -3,9 +3,13 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import httpClient from '@/api/httpClient';
 import { ENDPOINTS } from '@/config/endpoints.config';
 import { QUERY_KEYS } from '@/config/react-query.config';
+import { ROUTES } from '@/config/routes.config';
 import { AccommodationReq } from '@/types/accommodation.types';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 export const useCreateAccommodation = () => {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { mutate } = useMutation({
     mutationFn: async (data: AccommodationReq) => {
@@ -14,6 +18,8 @@ export const useCreateAccommodation = () => {
 
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.query.accommodations] });
+      toast.success('Accommodation created successfully');
+      navigate(ROUTES.accommodations.root);
     },
   });
 
--- src/api/mutations/accommodations/useUpdateAccommodation.ts
+++ src/api/mutations/accommodations/useUpdateAccommodation.ts
@@ -3,9 +3,13 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import httpClient from '@/api/httpClient';
 import { ENDPOINTS } from '@/config/endpoints.config';
 import { QUERY_KEYS } from '@/config/react-query.config';
+import { ROUTES } from '@/config/routes.config';
 import { AccommodationReq, AccommodationType } from '@/types/accommodation.types';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 export const useUpdateAccommodation = () => {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { mutate } = useMutation({
     mutationFn: (data: AccommodationReq & { id: string }) => {
@@ -20,6 +24,8 @@ export const useUpdateAccommodation = () => {
       await queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.query.accommodation, res.data?.data.id],
       });
+      toast.success('Accommodation updated successfully');
+      navigate(ROUTES.accommodations.root);
     },
   });
 
