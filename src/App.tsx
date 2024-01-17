c4b878aba61a1a46868ca35e4be8a8be9fb723aa
Zilola <44432264+zilola08@users.noreply.github.com>
2024-01-17 16:59:50 +0500

    feat(search): add localization line to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
+
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 import './index.css';
 
 import { queryClient } from './config/react-query.config';
+
 import Routes from './routes/routes';
 import ThemeWrapper from './theme';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeWrapper>
-        <Routes />
-        <Toaster />
-      </ThemeWrapper>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <ThemeWrapper>
+          <Routes />
+          <Toaster />
+        </ThemeWrapper>
+      </LocalizationProvider>
     </QueryClientProvider>
   );
 }
