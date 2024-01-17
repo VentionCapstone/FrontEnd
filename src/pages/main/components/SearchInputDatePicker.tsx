b32f9712a9145600ec3028a703c5f28a2b392743
Zilola <44432264+zilola08@users.noreply.github.com>
2024-01-17 17:08:18 +0500

    feat(search): add searchDatePicker component

---
+++ src/pages/main/components/SearchInputDatePicker.tsx
@@ -0,0 +1,58 @@
+import { DatePicker, MobileDatePicker } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
+import { mainStyles } from '../index.styles';
+import { SearchDatePickerProps } from '@/types/accommodation.types';
+
+const SearchInputDatePicker = ({
+  isMobile,
+  label,
+  date,
+  minDate,
+  setDate,
+  handleDateChange,
+  UtcTimeToLocal,
+}: SearchDatePickerProps) => {
+  if (!isMobile) {
+    return (
+      <DatePicker
+        label={label}
+        sx={mainStyles.searchInput}
+        slotProps={{
+          field: {
+            clearable: true,
+            onClear: () => setDate(''),
+            readOnly: true,
+          },
+          actionBar: {
+            actions: ['clear'],
+          },
+        }}
+        value={date !== '' ? UtcTimeToLocal(dayjs(date)) : null}
+        minDate={minDate}
+        onChange={handleDateChange}
+      />
+    );
+  }
+
+  return (
+    <MobileDatePicker
+      label={label}
+      sx={mainStyles.searchInput}
+      slotProps={{
+        field: {
+          clearable: true,
+          onClear: () => setDate(''),
+          readOnly: true,
+        },
+        actionBar: {
+          actions: ['clear'],
+        },
+      }}
+      value={date !== '' ? UtcTimeToLocal(dayjs(date)) : null}
+      minDate={minDate}
+      onChange={handleDateChange}
+    />
+  );
+};
+
+export default SearchInputDatePicker;
