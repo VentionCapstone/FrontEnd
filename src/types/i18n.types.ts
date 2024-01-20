8b3ddf61343aa601ba5bba20e86d95f74e167858
asadbekkholdarov <507asadali@gmail.com>
2024-01-20 13:43:45 +0500

    feat: added types i18n ts file

--- src/types/i18n.types.ts
+++ src/types/i18n.types.ts
@@ -25,3 +25,74 @@ export enum PaymentInfo {
   button_type_card = 'payment.button_type.card',
   button_type_cash = 'payment.button_type.cash',
 }
+
+export enum HomeUIInfo {
+  user_home_btn_account = 'home_ui.user_home_btn.account',
+  user_home_btn_mylistings = 'home_ui.user_home_btn.mylistings',
+  user_home_btn_logout = 'home_ui.user_home_btn.logout',
+  filters_button_filter = 'home_ui.filters_button.filter',
+  filters_button_price_price_range = 'home_ui.filters_button.price.price_range',
+  filters_button_price_price_description = 'home_ui.filters_button.price.price_description',
+  filters_button_rooms_any = 'home_ui.filters_button.rooms.any',
+  filters_button_people_any = 'home_ui.filters_button.people.any',
+  filters_button_sort_price = 'home_ui.filters_button.sort.price',
+  filters_button_sort_room = 'home_ui.filters_button.sort.room',
+  filters_button_sort_people = 'home_ui.filters_button.sort.people',
+  filters_button_clear_btn = 'home_ui.filters_button.clear_btn',
+  filters_button_filter_btn = 'home_ui.filters_button.filter_btn',
+  card_night = 'home_ui.card.night',
+  footer_info_terms = 'home_ui.footer_info.terms',
+  footer_info_sitemap = 'home_ui.footer_info.sitemap',
+  footer_info_privacy = 'home_ui.footer_info.privacy',
+  footer_info_your_privacy_choices = 'home_ui.footer_info.your_privacy_choices',
+  footer_info_copyright = 'home_ui.footer_info.copyright',
+}
+
+export enum AccountEditPageInfo {
+  personal_info = 'account_edit_page.personal_info',
+  personal_desc = 'account_edit_page.personal_desc',
+  login_secure = 'account_edit_page.login_secure',
+  login_secure_desc = 'account_edit_page.login_secure_desc',
+  setting = 'account_edit_page.setting',
+  setting_desc = 'account_edit_page.setting_desc',
+}
+
+export enum AccountEditPersonalInfo {
+  name = 'account_edit_personal-info.name',
+  legal_name = 'account_edit_personal-info.name.legal_name',
+  legal_name_desc = 'account_edit_personal-info.name.legal_name_desc',
+  edit_name = 'account_edit_personal-info.name.edit',
+  save_name = 'account_edit_personal-info.name.save',
+  first_name = 'account_edit_personal-info.name.first_name',
+  last_name = 'account_edit_personal-info.name.last_name',
+
+  gender = 'account_edit_personal-info.genger',
+  your_gender = 'account_edit_personal-info.genger.your_gender',
+  male = 'account_edit_personal-info.genger.male',
+  female = 'account_edit_personal-info.genger.famele',
+  edit_gender = 'account_edit_personal-info.genger.edit',
+  save_gender = 'account_edit_personal-info.genger.save',
+  select_gender = 'account_edit_personal-info.genger.select',
+
+  location = 'account_edit_personal-info.location',
+  where_do_live = 'account_edit_personal-info.location.where_do_live',
+  edit_location = 'account_edit_personal-info.location.edit',
+  save_location = 'account_edit_personal-info.location.save',
+  select_country = 'account_edit_personal-info.location.select',
+  uzbekistan = 'account_edit_personal-info.location.uzbekistan',
+  kazakhstan = 'account_edit_personal-info.location.kazakhstan',
+  russia = 'account_edit_personal-info.location.russian',
+
+  number = 'account_edit_personal-info.number',
+  phone_number = 'account_edit_personal-info.number.phone_number',
+  phone_number_desc = 'account_edit_personal-info.number.phone_number_desc',
+  edit_number = 'account_edit_personal-info.number.edit',
+  save_number = 'account_edit_personal-info.number.save',
+
+  about = 'account_edit_personal-info.about',
+  about_title = 'account_edit_personal-info.about.about',
+  about_desc = 'account_edit_personal-info.about.about_desc',
+  about_textarea = 'account_edit_personal-info.about.about_textarea',
+  edit_about = 'account_edit_personal-info.about.edit',
+  save_about = 'account_edit_personal-info.about.save',
+}
