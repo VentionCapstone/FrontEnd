51794ecffd2d6286cf2bc78aee61c78f9d5aac57
Zach13131 <125944644+Zach13131@users.noreply.github.com>
2024-02-01 02:52:11 +0500

    fix(update): add origin url to href for ballon

--- src/pages/main/components/MapModal.tsx
+++ src/pages/main/components/MapModal.tsx
@@ -61,7 +61,7 @@ export default function MapModal({ open, setOpen, searchParamsAsObject }: MapMod
         '<a class="close" href="#"><svg style="width: 26px;padding: 3px;height: 26px;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" viewBox="0 0 490 490" xml:space="preserve"><polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490   489.292,457.678 277.331,245.004 489.292,32.337 "/></svg></a>' +
         '<div class="arrow"></div>' +
         '<div class="popover-inner">' +
-        '<a class="ballon__link" href="http://localhost:5173/rooms/$[properties.accommodationId]">' +
+        `<a class="ballon__link" href="${originUrl}/rooms/$[properties.accommodationId]">` +
         '$[[options.contentLayout observeSize minWidth=300 maxWidth=300 maxHeight=450]]' +
         '</a>' +
         '</div>' +
