7ea99d37bec823129b556e94260032c6baf4d899
fasbrothers <nigmatovsanjar00@gmail.com>
2024-02-01 13:08:23 +0500

    feat: add confirm email page

--- src/api/mutations/auth/useSignupMutation.ts
+++ src/api/mutations/auth/useSignupMutation.ts
@@ -2,12 +2,18 @@ import { useMutation } from '@tanstack/react-query';
 
 import httpClient from '@src/api/httpClient';
 import { ENDPOINTS } from '@src/config/endpoints.config';
+import { ROUTES } from '@src/config/routes.config';
 import { SignUpReq } from '@src/types/auth.types';
+import { useNavigate } from 'react-router-dom';
 
 const useSignupMutation = () => {
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: async (values: SignUpReq) => {
       await httpClient.post(ENDPOINTS.auth.signUp, values);
+    },
+    onSuccess: () => {
+      navigate(ROUTES.auth.confirmEmail);
     },
   });
 };
