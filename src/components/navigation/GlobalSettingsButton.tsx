a28f1c27cfb94a6867c5dc610255c38abd48d62d
MdR <mr.slygrin@gmail.com>
2024-02-06 19:10:48 +0500

    feat: update user profile language if user is logged in

--- src/components/navigation/GlobalSettingsButton.tsx
+++ src/components/navigation/GlobalSettingsButton.tsx
@@ -1,16 +1,26 @@
 import LanguageIcon from '@mui/icons-material/Language';
 import { IconButton, List, ListItem, ListItemButton, Popover } from '@mui/material';
 import { useCallback, useState } from 'react';
+import toast from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
 
+import useEditAccountMutation from '@src/api/mutations/account/useEditAccountMutation';
 import { LOCAL_STORAGE_KEYS } from '@src/config/local-storage.config';
 import { LANGUAGE_LIST } from '@src/constants';
 import { LanguageCollection } from '@src/constants/constant.types';
+import { useAppSelector } from '@src/hooks/redux-hooks';
 import i18n from '@src/i18n/i18n';
+import { ErrorTypes } from '@src/types/i18n.types';
 import { setValueToLocalStorage } from '@src/utils';
 
 const GlobalSettingsButton = () => {
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
   const open = Boolean(anchorEl);
+  const { t } = useTranslation();
+
+  const profile = useAppSelector((state) => state.auth?.user?.profile);
+  const profileId = profile?.id ?? '';
+  const { mutate } = useEditAccountMutation(profileId);
 
   const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -20,18 +30,26 @@ const GlobalSettingsButton = () => {
     setAnchorEl(null);
   };
 
-  const handleLangChange = useCallback((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const dataset = event.currentTarget.dataset;
-    const code = dataset.code as LanguageCollection['code'];
+  const handleLangChange = useCallback(
+    async (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      const dataset = event.currentTarget.dataset;
+      const code = dataset.code as LanguageCollection['code'];
 
-    i18n
-      .changeLanguage(code)
-      .then(() => {
-        setValueToLocalStorage(LOCAL_STORAGE_KEYS.language, code);
+      try {
+        if (profile && code !== profile?.language) {
+          mutate({ language: code });
+        }
+
+        await i18n.changeLanguage(code);
         setAnchorEl(null);
-      })
-      .catch(console.error);
-  }, []);
+        setValueToLocalStorage(LOCAL_STORAGE_KEYS.language, code);
+      } catch (error) {
+        console.error(error);
+        toast.error(t(ErrorTypes.language_update));
+      }
+    },
+    [profile, mutate, t]
+  );
 
   return (
     <>
